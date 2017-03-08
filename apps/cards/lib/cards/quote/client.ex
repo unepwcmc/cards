37fdb9e367c9ab17d36993c09fdbea10edfa3f0c
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-03-08 15:36:27 +0000

    Add logger to print response when error happens in Cards.Quote.Client

--- apps/cards/lib/cards/quote/client.ex
+++ apps/cards/lib/cards/quote/client.ex
@@ -1,4 +1,5 @@
 defmodule Cards.Quote.Client do
+  require Logger
   @api_url "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json"
 
   def query do
@@ -11,8 +12,10 @@ defmodule Cards.Quote.Client do
 
   def format({:ok, %{"quoteText" => quote, "quoteAuthor" => author}}), do:
     {:ok, %{quote: quote, author: author}}
-  def format(_), do:
+  def format(response) do
+    Logger.info response
     {:error, :unexpected_response}
+  end
 
   defp remove_silly_characters(body) do
     String.replace(body, "\\'", "'")
