d42fccacf7351c4951b40dd8c1acd8d7fb5c446d
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-03-11 10:12:20 +0000

    add import of secrets if ENV.secret.exs file is present

--- apps/cards/config/config.exs
+++ apps/cards/config/config.exs
@@ -9,3 +9,10 @@ config :cards, Cards.Dribbble.Client,
   api_key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 import_config "#{Mix.env}.exs"
+
+# As a safer alternative, create a #{Mix.env}.secret.exs file, and set your config
+# in there (copy the config above and edit the values). That path is
+# gitignored, so it'll never end up being pushed to origin
+if __DIR__ |> Path.join("#{Mix.env}.secret.exs") |> File.exists? do
+  import_config "#{Mix.env}.secret.exs"
+end
