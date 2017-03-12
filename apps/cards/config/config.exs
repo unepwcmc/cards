70536f5f06805dbdb261902a4bedcb0dac64043a
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-03-12 22:10:50 +0000

    add Cards.Appsignal

--- apps/cards/config/config.exs
+++ apps/cards/config/config.exs
@@ -4,6 +4,11 @@ config :cards,
   quotes_api: Cards.Quote.Client,
   hacker_news_api: Cards.HackerNews.Client,
   dribbble_api: Cards.Dribbble.Client,
+  appsignal_api: Cards.Appsignal.Client,
   dribbble_api_key: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+config :cards, Cards.Appsignal.Client,
+  app_id: "xxxxxxxxxxxxxxxxxxxxxxxx",
+  token: "xxxxxxxxxxxxxxxxxxxxxx"
 
 import_config "#{Mix.env}.exs"
