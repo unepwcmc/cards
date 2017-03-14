e5666a594e8f95859a2916bff9e11c9be3eda5db
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2017-03-14 15:29:55 +0000

    add server: true to config for Cards.Web.Endpoint

--- apps/cards_web/config/prod.exs
+++ apps/cards_web/config/prod.exs
@@ -15,7 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cards_web, Cards.Web.Endpoint,
   on_init: {Cards.Web.Endpoint, :load_from_system_env, []},
-  url: [host: "cards.unep-wcmc.org", port: 80],
+  url: [host: "cards.unep-wcmc.org", port: 80], server: true,
+  version: Mix.Project.config[:version], root: ".",
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
