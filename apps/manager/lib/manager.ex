5a13fe982dcb8a3dfb815e3f7f8c9179b3910ff6
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2017-03-21 10:09:39 +0000

    10% chance the next card is a random one in the queue

--- apps/manager/lib/manager.ex
+++ apps/manager/lib/manager.ex
@@ -7,6 +7,11 @@ defmodule Manager do
   Returns the next card in Manager.Queue
   """
   def next do
-    Manager.Queue.next()
+    random_number = Enum.random(0..9)
+    if random_number == 0 do
+      Manager.Queue.random()
+    else
+      Manager.Queue.next()
+    end
   end
 end
